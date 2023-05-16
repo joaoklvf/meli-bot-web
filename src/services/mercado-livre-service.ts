bcfc55da957ae5f2b39ea8de993b36e76db5e248
João Figueredo <joaoklvf@gmail.com>
2023-05-16 20:43:14 -0300

    Adicionando para obter usuário teste

--- src/services/mercado-livre-service.ts
+++ src/services/mercado-livre-service.ts
@@ -2,8 +2,17 @@ import { api } from './api';
 
 export const getToken = async (refreshToken: string) => {
   try {
-    console.log(refreshToken, 'refreshToken')
     const { data } = await api.post('meli/token', null, { params: { code: refreshToken } });
+    return data;
+  }
+  catch (e: any) {
+    throw new Error(e);
+  }
+}
+
+export const getUserTest = async (token: string) => {
+  try {
+    const { data } = await api.post('meli/user-test', null, { params: { token } });
     return data;
   }
   catch (e: any) {
