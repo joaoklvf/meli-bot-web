d16fc522d4d4a14d6928dfe38ac79dc5eac876c3
João Figueredo <joaoklvf@gmail.com>
2023-05-22 00:18:03 -0300

    Automatizando tentativa de obter tokens

--- src/services/mercado-livre-service.ts
+++ src/services/mercado-livre-service.ts
@@ -3,17 +3,33 @@ import { api } from './api';
 export const getToken = async (refreshToken: string) => {
   try {
     const { data } = await api.post('meli/token', null, { params: { code: refreshToken } });
-    return data;
+    if (!!data.access_token)
+      return data;
+    window.location.href = ('https://auth.mercadolivre.com.br/authorization?response_type=code&client_id=7653033061543176&redirect_uri=http://localhost:5173/meli');
   }
   catch (e: any) {
     throw new Error(e);
   }
 }
 
-export const getUserTest = async (token: string) => {
+export const getUserTest = async (refreshToken: string | null, token: string, setToken: (value: string) => void) => {
+  let user;
   try {
     const { data } = await api.post('meli/user-test', null, { params: { token } });
-    return data;
+    console.log('data', data);
+    if (!!data.id) {
+      return data;
+    }
+    if (!refreshToken) {
+      throw new Error("Refresh token null!");
+    }
+    await getToken(refreshToken).then(async ({ access_token }) => {
+      setToken(access_token);
+      const newRequest = await api.post('meli/user-test', null, { params: { token: access_token } });
+      user = newRequest.data;
+    });
+
+    return user;
   }
   catch (e: any) {
     throw new Error(e);
