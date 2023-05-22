85195bb42bc07e3c2a4b3e9a1eedcc64f83f40fd
João Figueredo <joaoklvf@gmail.com>
2023-05-22 00:18:55 -0300

    Nova estrutura página MELI

--- src/pages/Meli/index.tsx
+++ src/pages/Meli/index.tsx
@@ -16,8 +16,8 @@ const handleToken = async (refreshToken: string | null) => {
   return tokenData.access_token;
 }
 
-const handleUserTest = async (token: string) => {
-  const testUser = await getUserTest(token);
+const handleUserTest = async (refreshToken: string | null, token: string, setToken: (value: string) => void) => {
+  const testUser = await getUserTest(refreshToken, token, setToken);
   return testUser;
 }
 
@@ -33,26 +33,33 @@ export function Meli() {
   }
 
   const handleOnGetUserTestClick = async () => {
-    const newTestUser = await handleUserTest(token);
+    const newTestUser = await handleUserTest(refreshToken, token, setToken);
     setTestUser(newTestUser);
   }
 
+  const handleOnInvalidarTokenButtonClick = () => {
+    setToken('token_invalido');
+  }
+
   return (
-    <div className="m-5">
-      <button type='button' onClick={handleOnGetTokenClick}>Obter token do Mercado Livre</button>
-      <h2 className="text-3xl font-bold underline">
-        Refresh Token: {refreshToken}
+    <div className="p-5">
+      <div className="flex gap-x-2">
+        <button type='button' className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' onClick={handleOnGetTokenClick}>Obter token do Mercado Livre</button>
+        <button type='button' className='bg-transparent hover:bg-gray-500 text-gray-700 font-semibold hover:text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded' onClick={handleOnInvalidarTokenButtonClick}>Invalidar token</button>
+      </div>
+      <h2 className="text-2xl font-bold">
+        <u>Refresh Token</u>: {refreshToken}
       </h2>
-      <h2 className="text-3xl font-bold underline">
-        Token: {token}
+      <h2 className="text-2xl font-bold">
+        <u>Token:</u> {token}
       </h2>
       {token && (
-        <div>
-          <button type='button' onClick={handleOnGetUserTestClick}>Obter usuário teste</button>
-          <div className="mt-1" style={{ marginTop: '1rem' }}>
-            {JSON.stringify(testUser)}
-          </div>
+      <div className="mt-4">
+        <button type='button' className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' onClick={handleOnGetUserTestClick}>Obter usuário teste</button>
+        <div className="mt-1">
+          {JSON.stringify(testUser)}
         </div>
+      </div>
       )}
     </div>
   );
