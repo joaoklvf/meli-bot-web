7217b3daa6f95d682124d58e0ffd617b664fc427
João Figueredo <joaoklvf@gmail.com>
2023-05-29 13:19:21 -0300

    Chamando listagem

--- src/pages/Meli/components/ProductsComponent/index.tsx
+++ src/pages/Meli/components/ProductsComponent/index.tsx
@@ -1,3 +1,4 @@
+import { ResponsiveTable } from "../../../../components/ResponsiveTable";
 import { useProduct } from "./useProduct";
 
 interface ProductsComponentProps {
@@ -8,7 +9,7 @@ export const ProductsComponent = ({ token }: ProductsComponentProps) => {
   const { product, setProduct, handlePostProduct } = useProduct({ token });
 
   return !token ? <></> :
-    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <div>
         <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Autor</label>
         <input onChange={(e) => setProduct(vl => ({ ...vl, authorName: e.target?.value }))} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Davi" />
@@ -19,7 +20,7 @@ export const ProductsComponent = ({ token }: ProductsComponentProps) => {
       </div>
       <button type='button' className='mt-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' onClick={handlePostProduct}>Enviar produto</button>
       <div className="mt-1 text-white">
-        {JSON.stringify(product)}
+        <ResponsiveTable data={[product]} />
       </div>
     </div>
 }
--- src/pages/Meli/components/SitesComponent/index.tsx
+++ src/pages/Meli/components/SitesComponent/index.tsx
@@ -1,3 +1,4 @@
+import { ResponsiveTable } from "../../../../components/ResponsiveTable";
 import { useSites } from "./useSites";
 
 interface SitesComponentProps {
@@ -6,12 +7,12 @@ interface SitesComponentProps {
 
 export const SitesComponent = ({ token }: SitesComponentProps) => {
   const { sites, handleGetSites } = useSites({ token });
-  console.log('token', token);
+
   return !token ? <></> :
     <div className="max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <button type='button' className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' onClick={handleGetSites}>Obter sites</button>
       <div className="mt-1 text-white">
-        {JSON.stringify(sites)}
+        <ResponsiveTable data={sites} />
       </div>
     </div>
 }
