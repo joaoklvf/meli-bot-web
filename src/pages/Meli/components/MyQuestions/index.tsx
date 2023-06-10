d9d3917ed60e81e5577058f8ede3c699d91cbb7b
João Figueredo <joaoklvf@gmail.com>
2023-06-10 15:56:41 -0300

    ajuste undefined

--- src/pages/Meli/components/MyQuestions/index.tsx
+++ src/pages/Meli/components/MyQuestions/index.tsx
@@ -9,7 +9,7 @@ export const MyQuestions = () => {
       <div>
         <button type='button' className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' onClick={handleGetOptions}>Recarregar busca</button>
       </div>
-      {questions.map(question => (
+      {questions?.map(question => (
         <QuestionComponent pergunta={question} key={question.id} />
       ))}
     </div>
