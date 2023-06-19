9b23af1014017e926ad61d3aadd25d6f21639d1c
tetzgabriel <tetz.gabriel@gmail.com>
2023-06-19 15:55:25 -0300

    Feature | Adding option to answer manually

--- src/pages/Meli/components/MyQuestions/Question/index.tsx
+++ src/pages/Meli/components/MyQuestions/Question/index.tsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import { Question } from "../../../../../interfaces/question";
-import { createChatGptAnswer, approveAnswer } from "../../../../../services/answer-service";
+import { createChatGptAnswer, approveAnswer, createUserAnswer } from "../../../../../services/answer-service";
 
 interface QuestionComponentProps {
   question: Question
@@ -12,8 +13,20 @@ const getDateString = (date: Date) => `${date.toLocaleDateString()} às ${date.t
 export const QuestionComponent = ({ question, setQuestions, questions }: QuestionComponentProps) => {
   const date = getDateString(new Date(question.date_created));
 
+  const [userAnswer, setuserAnswer] = useState('');
+
   const handleCreateChatGptAnswer = async (questionId: number) => {
     const newAnswer = await createChatGptAnswer(questionId);
+    const currentQuestions = questions.map(x => {
+      if (x.id === question.id)
+        x.answer = newAnswer;
+      return x;
+    });
+    setQuestions(currentQuestions);
+  }
+
+  const handleCreateManualAnswer = async (questionId: number, answer: string) => {
+    const newAnswer = await createUserAnswer(questionId, answer);
     const currentQuestions = questions.map(x => {
       if (x.id === question.id)
         x.answer = newAnswer;
@@ -70,7 +83,7 @@ export const QuestionComponent = ({ question, setQuestions, questions }: Questio
           </div>
           <hr />
           <div className="p-2 h-32">
-            {question.answer ?
+            {question.answer && question.answer.chatGptAnswer ?
               <div>
                 {!question.answer.approved &&
                   <button type='button' className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' onClick={() => handleCreateChatGptAnswer(question.id)}>Gerar resposta</button>}
@@ -87,6 +100,26 @@ export const QuestionComponent = ({ question, setQuestions, questions }: Questio
           </div>
         </div>
       </div>
+      <div className="border border-slate-700 py-2">
+          <div className="p-2">
+            Responder manualmente:
+          </div>
+          <hr />
+          <div className="p-2 h-32">
+            {question.answer && !question.answer.chatGptAnswer ?
+              <div>
+                <div className="mt-2">
+                  <p>{question.answer.answer}</p>
+                </div>
+              </div> :
+              <div>
+                <input type="text" className='bg-transparent border border-blue-500' value={userAnswer} onChange={e => setuserAnswer(e.target.value)}/>
+                <br/>
+                <button type='button' className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' onClick={() => handleCreateManualAnswer(question.id, userAnswer)}>Enviar resposta</button>
+              </div>
+            }
+          </div>
+        </div>
     </div >
   );
 }
